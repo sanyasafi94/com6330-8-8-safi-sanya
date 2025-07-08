26c1660da02d03a5be8d732d0157d671ed9eeb28
Sanya Safi <sanyasafi94@gmail.com>
2025-07-08 17:17:36 -0500

    API key added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var weatherApp = document.querySelector("#weather-app form");
 var weatherSearch = document.getElementById("weather-search");
 var weatherSection = document.getElementById("weather");
+var weatherAPI = "b1c77ffbed0a997975e7fe53c6a435d3 ";
